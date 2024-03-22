4b854775089faeca409a120ab96b8350582bf798
yosuke <yosuke20sasami@gmail.com>
2024-03-22 21:04:00 +0900

    setup_fin

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,11 @@ module RevengePortfolio
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.skip_routes true
+    end
   end
 end
