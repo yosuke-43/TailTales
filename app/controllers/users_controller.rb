c7cc702ee1a8063119a139118aba31519225e26c
yosuke <yosuke20sasami@gmail.com>
2024-03-24 01:39:14 +0900

    会員登録、ログイン機能実装完了

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      auto_login(@user)
+      redirect_back_or_to root_path, success: t('.success')
+    else
+      flash.now[:danger] = t('.fail')
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
