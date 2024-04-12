2cec5344a9e79fb20eb1977024d1f95d4ee1cc9b
yosuke <yosuke20sasami@gmail.com>
2024-04-12 21:03:41 +0900

    インクリメンタル検索できるように修正

--- app/controllers/dogs/lists_controller.rb
+++ app/controllers/dogs/lists_controller.rb
@@ -1,11 +1,12 @@
 class Dogs::ListsController < ApplicationController
+  skip_before_action :require_login, only: [:index]
   def index
     limited_breeds = params[:limited_breeds]
     
     if limited_breeds == 'limited_display'
-      @list_dogs = Dog.where('breed LIKE ? AND display_in_index = ?', "%#{params[:breed]}%", 1)
+      @list_dogs = Dog.where('breed LIKE ? AND display_in_index = ?', "%#{params[:breed]}%", 1).order(:breed)
     else
-      @list_dogs = Dog.where('breed LIKE ?', "%#{params[:breed]}%")
+      @list_dogs = Dog.where('breed LIKE ?', "%#{params[:breed]}%").order(:breed)
     end
     respond_to do |format|
       format.html { redirect_to :root }
